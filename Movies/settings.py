cc41c0d136dbe4c55106af83295afe6433f3e8e2
Tomasz Ficek <tomekficek22@gmail.com>
2020-10-30 13:32:05 +0100

    10/30/2020

--- Movies/settings.py
+++ Movies/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform', # dodanie zainstaowanej appki do stylizowania formularzy Django - "django bootstrap form"
     'filmyweb'
 ]
 
